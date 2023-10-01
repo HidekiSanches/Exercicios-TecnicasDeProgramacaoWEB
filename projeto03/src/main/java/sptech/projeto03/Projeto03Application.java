2d33d8084a8f6141bfb43cc6edea1d02227bd8a2
HidekiSanches <thiago.sanches@sptech.school>
2023-10-01 18:27:45 -0300

    Feat: Projeto 03

---
+++ projeto03/src/main/java/sptech/projeto03/Projeto03Application.java
@@ -0,0 +1,13 @@
+package sptech.projeto02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(sptech.projeto02.Projeto03Application.class, args);
+	}
+
+}
